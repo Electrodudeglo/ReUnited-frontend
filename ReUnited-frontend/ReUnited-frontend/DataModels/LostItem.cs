6032fed518d6fb5cd5768b60062c0f9cd1e709d3
Imad Ahmed <j166a775433@gmail.com>
2026-06-01 13:52:41 +0100

    Add data model

---
+++ ReUnited-frontend/ReUnited-frontend/DataModels/LostItem.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReUnited_frontend.DataModels
+{
+    public class LostItem
+    {
+        public int Id { get; set; }
+
+        public string City { get; set; } = string.Empty;
+
+        public string Postcode { get; set; } = string.Empty;
+
+        public string? Email { get; set; }
+
+        public string? PhoneNumber { get; set; }
+
+        public string Category { get; set; } = string.Empty;
+
+        public string ItemDescription { get; set; } = string.Empty;
+
+        public string AdditionalInformation { get; set; } = string.Empty;
+
+        public string Picture { get; set; } = string.Empty;
+
+    }
+}
