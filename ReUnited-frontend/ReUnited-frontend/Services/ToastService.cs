ed5c1511d622069200072dd05545401acfb40993
Mohamed <mohamedmohamed1812@gmail.com>
2026-06-07 18:46:16 +0100

    Add: Adding Service folder to project

---
+++ ReUnited-frontend/ReUnited-frontend/Services/ToastService.cs
@@ -0,0 +1,11 @@
+﻿namespace Services
+{
+    public static class ToastService
+    {
+        public static event Action<string>? OnShow;
+        public static void Show(string message)
+        {
+            OnShow?.Invoke(message);
+        }
+    }
+}
